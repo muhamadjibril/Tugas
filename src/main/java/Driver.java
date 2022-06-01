565f29fc3f5dce8bc6c82a8f6aeba38ec3bc7790
muhamadjibril <79740456+muhamadjibril@users.noreply.github.com>
2022-06-01 22:43:47 +0700

    Add files via upload

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,10 +1,21 @@
+import src.main.java.Counter;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		Shouter shouter = new Shouter();
 		
-		shouter.shout("hurrah");
+		Counter counter = new Counter();
+		
+		System.out.println("Count:" + counter.getCount());
+		
+		counter.increment();
+		
+		System.out.println("Count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Count:" + counter.getCount());
+		
 	}
 
 }
